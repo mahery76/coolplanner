0012388dc598efc1ee31502f49e264855aa4a5a4
mahery76 <randrianjafymahery@gmail.com>
2024-04-03 20:03:56 +0200

    feat: testing the majors api route

--- src/app/api/majors/route.tsx
+++ src/app/api/majors/route.tsx
@@ -2,17 +2,15 @@ import { NextResponse, NextRequest } from 'next/server'
 import { PrismaClient } from "@prisma/client";
 
 
-export async function GET(req: any){
-    
+export async function POST(req: any){
+    const body = await req.json()
+    console.log(body)
     const prisma = new PrismaClient();
     try {
-       
         const newRole = await prisma.majors.create({ 
-            data: {
-                major_name: "Computer Science",
-                user_id: "String"
-            },
+            data: body
         })
+        return NextResponse.json(newRole)
     } catch (err) {
         console.error(err)
     }
--- utils/todo.md
+++ utils/todo.md
@@ -1,4 +1,4 @@
 ## Admin interface
 - [x] basic interface with layout and navigation
-- [ ]  
+- [ ] input field in  
 - [ ] 
