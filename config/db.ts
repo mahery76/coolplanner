fe46a9ea9c8a9e8afb00a811eac12a2ee73bd3e4
mahery76 <randrianjafymahery@gmail.com>
2024-04-04 16:21:04 +0200

    feat: get and post api for user_roles

---
+++ config/db.ts
@@ -0,0 +1,16 @@
+import { Pool } from "pg"
+
+export const pool = new Pool({
+    user: "postgres",
+    password: "borditasy", // mahery desktop pg mdp
+    // password: "lesdapery", // mahery laptop pg mdp
+    host: "localhost",
+    port: 5432,
+    database: "coolplannerdb"
+});
+
+// const pool = new Pool({
+//     connectionString: "postgres://ejeryemploidb_user:mPopMVq6Vz84JJhchNoAfK7WRdaCiabt@dpg-cmee89v109ks73c4hteg-a.oregon-postgres.render.com/ejeryemploidb?sslmode=require"
+// })
+
+
