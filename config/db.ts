8b69dc834299c835c9db8e84cf40a0a37a4bfc70
Mahery Antonio <randrianjafymahery@gmail.com>
2024-04-13 16:40:55 +0300

    chore: inspect cloud pg database url string

--- config/db.ts
+++ config/db.ts
@@ -1,9 +1,7 @@
 import { Pool } from "pg"
 
-
 export const pool = new Pool({
     // connectionString: "postgres://ejeryemploidb_user:mPopMVq6Vz84JJhchNoAfK7WRdaCiabt@dpg-cmee89v109ks73c4hteg-a.oregon-postgres.render.com/ejeryemploidb?sslmode=require"
     connectionString: process.env.DATABASE_URL
 })
-
 
